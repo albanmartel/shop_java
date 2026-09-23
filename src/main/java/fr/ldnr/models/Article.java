8e15390be88d94273e9d8d41e01a9a70a5839edc
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-23 15:08:47 +0200

    CREATE: 'Article.java' ajout de la classe Article correspondante à la table Article de la base de données

---
+++ src/main/java/fr/ldnr/models/Article.java
@@ -0,0 +1,24 @@
+package fr.ldnr.models;
+
+import java.math.BigDecimal;
+import fr.ldnr.models.Category;
+
+/* POur bénéficier des getters et setters  */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data                  // Génère les getters, setters, toString(), equals() et hashCode()
+@NoArgsConstructor     // Génère le constructeur sans argument
+@AllArgsConstructor    // Génère le constructeur avec tous les arguments
+/**
+ * Classe pour gérer les articles du projet Shop
+ * Les paramètres correspondent aux champs des tables SQL
+ */
+public class Article {
+    private Integer idArticle;
+    private String description;
+    private String brand;
+    private BigDecimal unitaryPrice;
+    private Category category;
+}
