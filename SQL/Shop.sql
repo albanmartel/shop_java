0e8468ba7a89fc3cac418fd8ebbef3fb01edd785
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-23 08:43:03 +0200

    Renommer 'UnitaryPrice' en 'unitaryPrice' dans la table 'article'

--- SQL/Shop.sql
+++ SQL/Shop.sql
@@ -12,20 +12,20 @@ CREATE TABLE article (
 	idArticle			int(4)		PRIMARY KEY AUTO_INCREMENT,
 	description			varchar(30)	NOT NULL,
 	brand				varchar(30)	NOT NULL,
-	UnitaryPrice		float(8)	NOT NULL DEFAULT 0
+	unitaryPrice		float(8)	NOT NULL DEFAULT 0
 );
 
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('Souris' , 'Logitoch', 65 );
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('Clavier' , 'Microhard', 49.5 );
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('Systeme d''exploitation', 'Fenetres Vistouille', 150);
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('Tapis souris','Chapeau Bleu',5 );
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('Cle USB 8 To','Syno', 8 );
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('Laptop' , 'PH', 1199);
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('CD x 500' ,'CETME', 250);
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('DVD-R x 100','CETME', 99);
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('DVD+R x 100' ,'CETME', 105);
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('Batterie Laptop','PH', 80);
-INSERT INTO article ( description, brand, UnitaryPrice ) VALUES ('Casque Audio','Syno',	105);
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('Souris' , 'Logitoch', 65 );
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('Clavier' , 'Microhard', 49.5 );
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('Systeme d''exploitation', 'Fenetres Vistouille', 150);
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('Tapis souris','Chapeau Bleu',5 );
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('Cle USB 8 To','Syno', 8 );
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('Laptop' , 'PH', 1199);
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('CD x 500' ,'CETME', 250);
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('DVD-R x 100','CETME', 99);
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('DVD+R x 100' ,'CETME', 105);
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('Batterie Laptop','PH', 80);
+INSERT INTO article ( description, brand, unitaryPrice ) VALUES ('Casque Audio','Syno',	105);
 INSERT INTO article ( description, brand ) VALUES ('WebCam', 'Logitoch');
 
 CREATE TABLE categorie (
@@ -64,7 +64,7 @@ CREATE TABLE order_item (
 	idArticle         INT(4)   NOT NULL,
     idOrder           INT(4)   NOT NULL,
 	Quantity		   FLOAT(4) NOT NULL DEFAULT 1,
-	UnitaryPrice	FLOAT(4)	NOT NULL DEFAULT 0,
+	unitaryPrice	FLOAT(4)	NOT NULL DEFAULT 0,
     FOREIGN KEY(idArticle) REFERENCES article(idArticle),
 	FOREIGN KEY(IdOrder) REFERENCES order(IdOrder)
 );
@@ -72,9 +72,9 @@ CREATE TABLE order_item (
 -- ALTER TABLE article ADD COLUMN IdCategory INT(4);
 -- ALTER TABLE article ADD FOREIGN KEY(IdCategory) REFERENCES categorie(IdCategory);
 
--- select idArticle,article.description,brand,UnitaryPrice,article.IdCategory,CatName,categorie.description 
+-- select idArticle,article.description,brand,unitaryPrice,article.IdCategory,CatName,categorie.description 
 -- from article inner join categorie where article.IdCategory = categorie.IdCategory and idArticle=1;
 
--- SELECT idArticle,article.description,brand,UnitaryPrice,CatName FROM article 
--- INNER JOIN categorie WHERE article.IdCategory=categorie.IdCategory AND idArticle>10 ORDER BY UnitaryPrice;
+-- SELECT idArticle,article.description,brand,unitaryPrice,CatName FROM article 
+-- INNER JOIN categorie WHERE article.IdCategory=categorie.IdCategory AND idArticle>10 ORDER BY unitaryPrice;
 -- SELECT * FROM article;
