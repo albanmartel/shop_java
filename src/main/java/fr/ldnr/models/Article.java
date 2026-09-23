99135f3ebf6456dc9ccc0d77bd79efbfc56a020d
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-23 18:17:08 +0200

    UPDATE: 'Article.java' déplacer les commentaires de classes et les annotation lombok

--- src/main/java/fr/ldnr/models/Article.java
+++ src/main/java/fr/ldnr/models/Article.java
@@ -8,13 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data                  // Génère les getters, setters, toString(), equals() et hashCode()
-@NoArgsConstructor     // Génère le constructeur sans argument
-@AllArgsConstructor    // Génère le constructeur avec tous les arguments
 /**
  * Classe pour gérer les articles du projet Shop
  * Les paramètres correspondent aux champs des tables SQL
  */
+@Data                  // Génère les getters, setters, toString(), equals() et hashCode()
+@NoArgsConstructor     // Génère le constructeur sans argument
+@AllArgsConstructor    // Génère le constructeur avec tous les arguments
 public class Article {
     private Integer idArticle;
     private String description;
