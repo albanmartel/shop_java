69d45ad024ef5645b4c7007e3aef326d76d2679d
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-23 23:02:18 +0200

    Tentative d'adaptation de TestArticleDao pour faire des tests

--- src/main/java/fr/ldnr/jdbc/TestArticleDao.java
+++ src/main/java/fr/ldnr/jdbc/TestArticleDao.java
@@ -2,13 +2,17 @@ package fr.ldnr.jdbc;
 
 import java.io.NotActiveException;
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Optional;
 
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
 import org.jooq.impl.QOM.Null;
 
 import fr.ldnr.dao.ArticleDao;
-import fr.ldnr.models.Article;
-import lombok.ToString;
+import fr.ldnr.shop.jooq.tables.records.ArticleRecord;
+
 
 /**
  * L'objectif de cette classe est de tester la classe ArticleDao et la classe Article
@@ -17,11 +21,7 @@ public class TestArticleDao {
 	
 	public static void testCreateArticleDao(ArticleDao articleDao) {
 		System.out.println("--- Test Insertion ---");
-        Article nouveauArticle = new Article();
-        nouveauArticle.setDescription("Clavier Mécanique");
-        nouveauArticle.setBrand("Logitoch");
-        nouveauArticle.setUnitaryPrice(new BigDecimal(89.99));
-        Article insere = articleDao.create(nouveauArticle);
+		ArticleRecord insere = articleDao.create("Clavier Mécanique", "Logitoch", new BigDecimal(89.99));
         System.out.println("Article inséré : " + insere);
 	}
 	
@@ -41,7 +41,7 @@ public class TestArticleDao {
 	
 	public static void testDisplayArticleDetails(ArticleDao articleDao) {
 		System.out.println("--- Test d'affichage du détail d'un article ---");
-		Optional<Article> article = articleDao.findById(1);
+		ArticleRecord article = articleDao.findById(1);
 		String messageString = "Pas d'article à afficher";
 		if (article != null) {
 			messageString = article.toString();
@@ -50,7 +50,16 @@ public class TestArticleDao {
 	}
 
 	public static void main(String[] args) {
-		ArticleDao articleDao = new ArticleDao();
+		
+		// 1. Connexion JDBC classique
+		Connection conn = DriverManager.getConnection(url, user, password);
+		
+		// 2. Création du DSLContext jOOQ
+		DSLContext dsl = DSL.using(conn, SQLDialect.MARIADB);
+
+		// 3. Instanciation de ton DAO avec le DSLContext
+		ArticleDao articleDao = new ArticleDao(dsl);
+
 		testCreateArticleDao(articleDao);
 		testDeleteArticleDao(articleDao);
 		testDisplayArticleDetails(articleDao);
