e19e6652b8c71505611becb8b0de3beab8b1d6d1
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-23 19:59:51 +0200

    UPDATE: 'App.java' modification du Hello word original en fichier de test

--- src/main/java/fr/ldnr/App.java
+++ src/main/java/fr/ldnr/App.java
@@ -1,10 +1,13 @@
 package fr.ldnr;
 
+import fr.ldnr.jdbc.TestArticleDao;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        TestArticleDao.main(args);        
     }
 }
