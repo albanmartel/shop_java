f4fcc6a553b3dab9053240141363dd14cfc7c027
Alban Martel <alban.f.j.martel@laposte.net>
2026-09-24 21:44:18 +0200

    mend
    Changement pour corriger le mélange des threads, création de 2 paramrètres différents pour le test 1 et le test2, system.out.print plutôt qu'un stockage

--- src/main/java/fr/ldnr/App.java
+++ src/main/java/fr/ldnr/App.java
@@ -1,15 +1,17 @@
 package fr.ldnr;
 
+import fr.ldnr.ex3threads.TestThread;
 import fr.ldnr.jdbc.TestArticleDao;
-import fr.ldnr.ex3threads.ThreadTime;
 
 public class App {
 	public static void exercice1(String[] args) {
+        System.out.println("Exercice 1");
 		TestArticleDao.main(args); 
 	}
 	
 	public static void exercice3(String[] args) {
-		ThreadTime.main(args); 
+        System.out.println("Exercice 3");
+		TestThread.main(args);
 	}
 	
     public static void main(String[] args) {
