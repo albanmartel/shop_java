56a5dd33b199721a525cb10ca2aa7a569ec6af14
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-24 11:14:33 +0200

    modification du fichier ThreadTime

--- src/main/java/fr/ldnr/App.java
+++ src/main/java/fr/ldnr/App.java
@@ -1,11 +1,21 @@
 package fr.ldnr;
 
 import fr.ldnr.jdbc.TestArticleDao;
+import fr.ldnr.ex3threads.ThreadTime;
 
 public class App {
+	public static void exercice1(String[] args) {
+		TestArticleDao.main(args); 
+	}
+	
+	public static void exercice3(String[] args) {
+		ThreadTime.main(args); 
+	}
+	
     public static void main(String[] args) {
         System.out.println("Manipuler une BDD sous éclipse");
-        TestArticleDao.main(args); 
+        exercice1(args);
+        exercice3(args);
         System.out.println("Fin du programme !");        
     }
 }
