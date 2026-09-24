bdd99688fe74e06dc9c778ca16a650f79ca0a362
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-24 13:52:43 +0200

    UPDATE: 'TestThread.java' création d'une seconde méthode pour répondre à l'exercice 3.2

--- src/main/java/fr/ldnr/ex3threads/TestThread.java
+++ src/main/java/fr/ldnr/ex3threads/TestThread.java
@@ -28,27 +28,47 @@ public class TestThread extends Thread {
      * @param i indique l'indice du theard
      * @return un chaîne construite
      */
-    public static String returnMotiv(int i){
+    public static String returnFirstMotiv(int i) {
         /* Méthode Java 11 */
         //return i + "-".repeat(i) + " ";
 
         /* Méthode avant Java 11 */
         String motiv = String.valueOf(i);
-        for (int j=0;j<i;j++){
+        for (int j = 0; j < i; j++) {
             motiv += "-";
         }
         return motiv + " ";
     }
 
     /**
+     *
+     * @param i indique l'indice du theard
+     * @return un chaîne construite
+     */
+    public static String returnSecondMotiv(String beginEndCharacter, int i) {
+        /* Méthode Java 11 */
+        //return i + "-".repeat(i) + " ";
+
+        /* Méthode avant Java 11 */
+        String motiv = beginEndCharacter;
+        for (int j = 0; j < i; j++) {
+            motiv += "*";
+        }
+        motiv += beginEndCharacter;
+
+        return motiv;
+    }
+
+
+    /**
      * Méthode déléguée du main
      */
-    public static void threadExecution() {
-        TestThread TestThread1 = new TestThread(returnMotiv(1));
-        TestThread TestThread2 = new TestThread(returnMotiv(2));
-        TestThread TestThread3 = new TestThread(returnMotiv(3));
-        TestThread TestThread4 = new TestThread(returnMotiv(4));
-        TestThread TestThread5 = new TestThread(returnMotiv(5));
+    public static void firstThreadExecution() {
+        TestThread TestThread1 = new TestThread(returnFirstMotiv(1));
+        TestThread TestThread2 = new TestThread(returnFirstMotiv(2));
+        TestThread TestThread3 = new TestThread(returnFirstMotiv(3));
+        TestThread TestThread4 = new TestThread(returnFirstMotiv(4));
+        TestThread TestThread5 = new TestThread(returnFirstMotiv(5));
         TestThread5.start();
         TestThread4.start();
         TestThread3.start();
@@ -65,14 +85,98 @@ public class TestThread extends Thread {
     }
 
     /**
+     * Méthode pour répondre à la quesion
+     */
+    public static void secondthreadExecution() {
+        TestThread TestThread1 = new TestThread(returnSecondMotiv("!", 1));
+        TestThread TestThread2 = new TestThread(returnSecondMotiv("\"", 2));
+        TestThread TestThread3 = new TestThread(returnSecondMotiv("#", 3));
+        TestThread TestThread4 = new TestThread(returnSecondMotiv("$", 4));
+        TestThread TestThread5 = new TestThread(returnSecondMotiv("%", 5));
+        TestThread TestThread6 = new TestThread(returnSecondMotiv("&", 6));
+        TestThread TestThread7 = new TestThread(returnSecondMotiv("'", 7));
+        TestThread TestThread8 = new TestThread(returnSecondMotiv("(", 8));
+        TestThread TestThread9 = new TestThread(returnSecondMotiv(")", 9));
+        TestThread TestThread10 = new TestThread(returnSecondMotiv("*", 10));
+
+
+        TestThread1.start();
+
+        try {
+            TestThread1.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        TestThread2.start();
+
+        try {
+            TestThread2.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        TestThread3.start();
+
+        try {
+            TestThread3.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        try {
+            TestThread4.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        TestThread5.start();
+
+        try {
+            TestThread5.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        TestThread6.start();
+
+        try {
+            TestThread6.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        TestThread7.start();
+
+        try {
+            TestThread7.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        TestThread8.start();
+        try {
+            TestThread8.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        TestThread9.start();
+        try {
+            TestThread9.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        TestThread10.start();
+    }
+
+    /**
      * Méthode appelée par défaut, le point d'entrée de la classe
+     *
      * @param args Tableau de Strings
      */
     public static void main(String[] args) {
         System.out.println("Lancement des threads!");
-        threadExecution();
+        firstThreadExecution();
         System.out.println("Pourquoi en relançant les mêmes threads j'ai un résultat différent : \n");
-        threadExecution();
-        System.out.println("Parce qu'il y a de la concurence entre les Threads !");
+        firstThreadExecution();
+        System.out.println("Parce qu'il y a de la concurrence entre les Threads !");
+        secondthreadExecution();
     }
 }
