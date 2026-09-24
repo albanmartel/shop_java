0f65a8f4bfc85df861c5ec62001ff1bb4bcc3d2b
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-24 11:37:31 +0200

    UPDATE: modification de la classe TestThread pour afficher ce qui est demandé.

--- src/main/java/fr/ldnr/ex3threads/TestThread.java
+++ src/main/java/fr/ldnr/ex3threads/TestThread.java
@@ -18,8 +18,18 @@ public class TestThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(this.getName());
+            System.out.print(this.getName());
         }
+        System.out.println();
+    }
+
+    /**
+     *
+     * @param i indique l'indice du theard
+     * @return un chaîne construite
+     */
+    public static String returnMotiv(int i){
+        return i + "-".repeat(i) + " ";
     }
 
     /**
@@ -27,16 +37,24 @@ public class TestThread extends Thread {
      * @param args Tableau de Strings
      */
     public static void main(String[] args) {
-        TestThread firstTestThreadFirst = new TestThread("-A-");
-        TestThread secondTestThreadFirst = new TestThread("----B----");
-        firstTestThreadFirst.start();
-        secondTestThreadFirst.start();
+        TestThread TestThread1 = new TestThread(returnMotiv(1));
+        TestThread TestThread2 = new TestThread(returnMotiv(2));
+        TestThread TestThread3 = new TestThread(returnMotiv(3));
+        TestThread TestThread4 = new TestThread(returnMotiv(4));
+        TestThread TestThread5 = new TestThread(returnMotiv(5));
+        TestThread5.start();
+        TestThread4.start();
+        TestThread3.start();
+        TestThread2.start();
+        TestThread1.start();
 
+        /*
         Thread thirdThread = new Thread() {
             public void run() {
                 System.out.println("!!! Autre ecriture!!!");
             }
         };
         thirdThread.start();
+         */
     }
 }
