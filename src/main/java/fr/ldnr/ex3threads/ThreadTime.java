cf98f86019eb4f142e1d3fee10d2c4acf4802378
Alban MARTEL <alban.martel@fms-ea.com>
2026-09-24 11:16:33 +0200

    Mise à jour du threa d

--- src/main/java/fr/ldnr/ex3threads/ThreadTime.java
+++ src/main/java/fr/ldnr/ex3threads/ThreadTime.java
@@ -1,38 +1,64 @@
-package fr.ldnr.thread;
+package fr.ldnr.ex3threads;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ *
+ */
+/**
+ *
+ */
+/**
+ *
+ */
+
+/**
+ *
+ */
 public class ThreadTime {
 
-  public static void main(String[] args) {
-    DateFormat df = new SimpleDateFormat("HH:mm:ss");
-    Thread thread = new Thread(new MonRunnable(1000));
+    public static void main(String[] args) {
+        DateFormat df = new SimpleDateFormat("HH:mm:ss");
+        Thread thread = new Thread(new MonRunnable(1000));
 
-    System.out.println(df.format(new Date()));
+        System.out.println(df.format(new Date()));
 
-    thread.start();
-  }
-
-  private static class MonRunnable implements Runnable {
-
-    private long delai;
-
-    public MonRunnable(long delai) {
-      this.delai = delai;
+        thread.start();
     }
 
-    @Override
-    public void run() {
-    	while(true) {
-	      try {
-	        Thread.sleep(delai);
-	        System.out.println("-");
-	      } catch (InterruptedException e) {
-	        e.printStackTrace();
-	      }
-    	}
+    /**
+     * Classe implémente l'interface Runnable
+     *
+     */
+    private static class MonRunnable implements Runnable {
+
+        private long delai;
+
+        /**
+         *
+         * @param delai traduit le temps en millisecondes
+         */
+        public MonRunnable(long delai) {
+            this.delai = delai;
+        }
+
+
+        @Override
+        public void run() {
+            System.out.println("Début du processus");
+            int i = 1;
+            while (i < 6) {
+                try {
+                    Thread.sleep(delai);
+                    System.out.println(i + "-".repeat(i));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                i++;
+            }
+            System.out.println("Fin du processus");
+        }
     }
-  }
 }
